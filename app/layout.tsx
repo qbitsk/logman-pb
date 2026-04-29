6fd959afa4855f685cc3c5492242421b52ad9fed
Juraj <juraj@macbook.pro>
2026-04-29 19:23:10 +0200

    add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "My App",
+  title: "Logman PB",
   description: "Data submission and management platform",
 };
 
