a9e48517808f48650e1867110b1cc4d33dda8ae4
Juraj <juraj@macbook.pro>
2026-04-29 16:00:50 +0200

    add sidebar menu button fix on mobile

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -8,7 +8,7 @@ export default function AdminLayout({
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 p-6 md:p-8 max-w-5xl">{children}</main>
+      <main className="flex-1 p-6 pt-16 md:p-8 md:pt-8 max-w-5xl">{children}</main>
     </div>
   );
 }
