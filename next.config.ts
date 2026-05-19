eb201834ce05a9a2ec59459b3f63da694dc3b363
Juraj <juraj@macbook.pro>
2026-05-19 11:50:20 +0200

    minor changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["exceljs"],
-  allowedDevOrigins: ['192.168.10.173'],
+  allowedDevOrigins: ['192.168.10.174'],
 };
 
 export default nextConfig;
